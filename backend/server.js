4cc713c2701de693ef6d5b4aa5d6b42f0d737431
Donalddddddd <161224579+Donalddddddd@users.noreply.github.com>
2025-10-14 19:15:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://simple-weather-lovat.vercel.app' 
+    'https://simple-weather-green-sigma.vercel.app' 
   ],
   credentials: true
 }));
